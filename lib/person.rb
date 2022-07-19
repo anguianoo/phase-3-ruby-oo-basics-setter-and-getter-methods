172479646179cedf8be7ab09c45ed99e00e88902
anguianoo <njanguiano@gmail.com>
2022-07-19 15:34:32 -0700

    added getter and setter methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
-
+  # Define a name getter and a name= setter method for the instance variable @name.
+  # Define a job getter and a job= setter method for the instance variable @job.
+  
+ # getter
+  def name
+    @name
+  end
+  
+  # setter
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
 end
